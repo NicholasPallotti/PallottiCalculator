35ab8f740678cf3b23d184b4fe5b36fb3672e752
Nicholas Pallotti <tcnp1996@gmail.com>
2019-10-01 04:00:26 -0600

    Order of operstions additons
    got rid of sommented out code, tried to get the calculator to do order of operations, currently doesnt work but close (if something liek 3/8 *4 is used the program will try and parse the string "3/8" as an double)

--- src/application/CalculatorController.java
+++ src/application/CalculatorController.java
@@ -8,17 +8,14 @@ import javafx.scene.control.TextField;
 public class CalculatorController {
 	
 	//the string input keeps track of what the user has input
-	private String input = "";
-	
-	//a placeholder variable for the answer
-	private double answer; 
+	private String input = "";	
 	
 	//answerBox is where the current equation to be solved is placed, and the answer when the equals button is clicked
 	@FXML
 	private TextField answerBox;
 	
 	
-	//i'm tired and not going to comment all these buttons, each button method says which button being pressed it handles, and
+	//each button method says which button being pressed it handles, and
 	//will add that number/operator to the input string and redisplay the new input string
 	@FXML
 	private void button1Clicked(ActionEvent event) {
@@ -115,146 +112,15 @@ public class CalculatorController {
 	@FXML 
 	private void buttonCalculateClicked(ActionEvent event) {
 		
-		//create a character array of the input split into characters
-		char[] inputTokens = input.toCharArray();
+		//create a new Calculator model instance with the input called answer
+		CalculatorModel answer = new CalculatorModel(input);
 		
-		//call the do calculation method which will return the answer
-		answer = doCalculation(inputTokens, 0);
-	
+		//call the do calculation method on answer and display it as a String
+		answerBox.setText(String.valueOf(answer.doCalculation()));
+				
 		//reset the input so the user can continue using the calculator if they want
-		input = "";
-		
-		//display the answer in the text box
-		answerBox.setText(String.valueOf(answer));
-	}
-
-	//method for performing the calculation
-	public double doCalculation(char inputTokens[], int index) {
-		
-		//a string for building the current number, between the operators
-		String currentNumber = "";
-		
-		//a double to convert the string currentNumber to
-		double number = 0;
-		
-		//a character to hold which operator is to be used
-		char operator = ' ';
-
-		//while loop to build the currntNumber String (while the index is a number and not an operator)	
-		while(inputTokens[index] != '*' && inputTokens[index] != '/' && inputTokens[index] != '+' && inputTokens[index] != '-') {
-			
-			//concat the digit at inputTokens current index to currentNumber and increment index 
-			currentNumber = currentNumber.concat(Character.toString( inputTokens[index]) );
-			index ++;
-			
-			//set number equal to the value of current number
-			number = Double.valueOf(currentNumber);
-			
-			//if the method has looped through the entire array of input tokens then return just number
-			if (index > inputTokens.length - 1) {
-				return number;
-			}
-		}
-		
-		//set the next digit to be the operator and increment index
-		operator = inputTokens[index];
-		index ++;
-		
-		//switch statement to perform the correct ooperator, will recursively call doCalculation with inputTokens and the current index
-		//until input index has been looped through
-		
-		switch (operator) {
-			case '*':
-				return number * doCalculation(inputTokens, index);
-				
-			case '/':
-				return number / doCalculation(inputTokens, index);
-				
-			case '+':
-				return number + doCalculation(inputTokens, index);
-				
-			case '-':
-				return number - doCalculation(inputTokens, index);
-				
-		}
-		
-		//eclipse was yelling at me to have a return statement so heres one that will never be called
-		return operator;
+		input = "";	
 		
 	}
-	
-	
-	
-	/*
-	
-	I couldnt figure out how to  do order or operations ill try and fix it
-	
-	public double subtraction(String inputString) {
-		
-		double answer = 0;
-		
-		String[] placeholder = inputString.split("\\-");
-		
-	
-		for(int i = 0; i < placeholder.length - 1; i++) {
-			answer = addition(placeholder[i]) - addition(placeholder[i + 1]);
-		}
-		
-		System.out.println("subtr " + answer);
-		return answer;
-	}
-	
-	public double addition(String inputString) {
-		
-		double answer = 0;
-		
-		String[] placeholder = inputString.split("\\+");
-		
-		
-			
-		
-		for(int i = 0; i < placeholder.length - 1; i++) {
-			answer =+ division(placeholder[i] + division(placeholder[i + 1]));
-		}
-		
-		
-		
-	
-		System.out.println("addiiton " + answer);
-		return answer;
-		
-	}
-	
-	public double division(String inputString) {
-		double answer = 0;
-		
-		String[] placeholder = inputString.split("\\/");
-		
-		
-		for(int i = 0; i < placeholder.length - 1; i++) {
-			answer = multiplication(placeholder[i]) / multiplication(placeholder[i + 1]) ;
-		}
-	
-				
-		System.out.println("division " + answer);
-		return answer;
-	}
-	
-	public double multiplication(String inputString) {
-		double answer = 0;
-		String[] placeholder = inputString.split("\\*");
-			
-		for(int i = 0; i < placeholder.length - 1; i++) {
-			answer = Double.valueOf(placeholder[i]) * Double.valueOf(placeholder[i + 1]);
-		}
-		
-		System.out.println("multip " + answer);
-		return answer;
-	}
-	
-
-	*/
-
-	
 
 }
--- src/application/CalculatorModel.java
+++ src/application/CalculatorModel.java
@@ -1,5 +1,180 @@
 package application;
 
 public class CalculatorModel {
+	
+	//class variables
+	private String input;
+	private double answer;
+	
+	//booleans that keep track of whether an operator is being used
+	boolean subtraction;
+	boolean addition;
+	boolean division;
+	boolean multiplication;
+	
+	
+	//Constructor for Calculator model
+	CalculatorModel(String input){
+		this.input = input;
+		
+		//finds what operators will be needed and for the operators needed sets the corresponding boolean to true
+		if(input.contains("-")) {
+			subtraction = true;
+			System.out.println("sub true");
+		}
 
+		if(input.contains("+")) {
+			addition = true;
+			System.out.println("add true");
+		}
+
+		if(input.contains("/")) {
+			division = true;
+			System.out.println("div true");
+		}
+
+		if(input.contains("*")) {
+			multiplication = true;
+			System.out.println("mult true");
+		}
+		
+	}
+	
+	//method for doing the calculation
+	public double doCalculation() {
+		
+		//in order to make the programming easier the program will skip the methods of unused operators
+		//the lowest level operator the input string contains (pemdas going backwards) is used first
+		if(subtraction == true) {
+			return answer = subtraction(input);
+		}
+		
+		else if(addition == true) {
+			return answer = addition(input);
+		}
+		
+		else if(division == true) {
+			return answer = division(input);
+		}
+		
+		else if(multiplication == true) {
+			return answer = multiplication(input);
+		}
+		//this else statement is in case the user only enters a number with no operators
+		else {
+			return Double.valueOf(input);
+		}
+		
+	}
+	
+	//method for doing subtraction
+	public double subtraction(String inputString) {
+		
+		//split the input string into sub blocks that need to be subtracted from each other
+		String[] placeHolder = inputString.split("\\-");
+		
+		double answerHolder = Double.valueOf(placeHolder[0]);
+		
+		//like earlier the program only calls calculation methods it needs to use
+		//i starts at 1 since we set answerHolder equal to placeHolder[0]
+		if(addition == true) {
+			for(int i = 1; i < placeHolder.length; i++) {
+				answerHolder -= addition(placeHolder[i]);
+			}
+		}
+		
+		else if(division == true) {
+			for(int i = 1; i < placeHolder.length; i++) {
+				answerHolder -= division(placeHolder[i]);
+			}
+		}
+		
+		else if(multiplication == true) {
+			for(int i = 1; i < placeHolder.length; i++) {
+				answerHolder -= multiplication(placeHolder[i]);
+			}
+		}
+		
+		//the else is in case there is only subtraction
+		else {
+			for(int i = 1; i < placeHolder.length; i++) {
+				answerHolder -= Double.valueOf(placeHolder[i]);
+			}
+		}
+
+		return answerHolder;
+	}
+	
+	//method for doing addition
+	public double addition(String inputString) {
+		
+		double answerHolder = 0;
+		
+		//split the input string into sub blocks that need to be added to each other
+		String[] placeHolder = inputString.split("\\+");
+		
+		//like earlier the program only calls calculation methods it needs to use
+		if(division == true) {
+			for(int i = 0; i < placeHolder.length; i++) {
+				answerHolder += division(placeHolder[i]);
+			}
+		}
+		
+		else if(multiplication == true) {
+			for(int i = 0; i < placeHolder.length; i++) {
+				answerHolder += multiplication(placeHolder[i]);
+			}
+		}
+		
+		//the else is in case there is only addition
+		else {
+			for(int i = 0; i <placeHolder.length; i++) {
+				answerHolder += Double.valueOf(placeHolder[i]);
+			}
+		}
+		
+		return answerHolder;
+		
+	}
+	
+	//method for performing division
+	public double division(String inputString) {
+			
+		String[] placeHolder = inputString.split("\\/");
+		
+		//split the input string into sub blocks that need to be divided by each other
+		double answerHolder = Double.valueOf(placeHolder[0]);
+		
+		//like earlier the program only calls calculation methods it needs to use
+		if(multiplication == true) {
+			for(int i = 1; i < placeHolder.length; i++) {
+				answerHolder /= multiplication(placeHolder[i]);
+			}
+		}
+		
+		//the else is in case no multiplication is needed
+		else {
+			for(int i = 1; i <placeHolder.length; i++) {
+				answerHolder /= Double.valueOf(placeHolder[i]);
+			}
+		}
+		
+		return answerHolder;
+	}
+	
+	//method for performing multiplication
+	public double multiplication(String inputString) {
+		
+		double answerHolder = 1;
+		
+		//split the input string into sub blocks that need to be divided by each other
+		String[] placeHolder = inputString.split("\\*");
+		
+		//see we are at the last level of operators we dont need to check if any other operators need to be done and can just do the multiplication
+		for(int i = 0; i <placeHolder.length; i++) {
+			answerHolder *= Double.valueOf(placeHolder[i]);
+		}
+
+		return answerHolder;
+	}
 }
